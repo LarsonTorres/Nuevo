a29f4267bfa7b92c5aa893a8a11942e241d258e7
LarsonTorres <duncanlarson07gmail.com>
2023-08-18 00:30:20 -0500

    LarsonTorres/nuevo

---
+++ resources/views/index.blade.php
@@ -0,0 +1,249 @@
+@extends('layout')
+@section('content')
+
+
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1 data-aos="fade-up">PROYECTO SENA-LARAVEL</h1>
+          <h2 data-aos="fade-up" data-aos-delay="400"></h2>
+          <div data-aos="fade-up" data-aos-delay="800">
+            <a href="#ubicacion" class="btn-get-started scrollto">SIGUIENTE</a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
+          <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+
+   <!-- ======= Portfolio Section ======= -->
+   <section id="ubicacion" class="portfolio">
+    <div class="container">
+
+      <div class="section-title" data-aos="fade-up">
+        <h2>SENA TIC</h2>
+        <p>Le regalamos una vista espectacular de nuestra sede</p>
+      </div>
+
+      <div class="row" data-aos="fade-up" data-aos-delay="200">
+        <div class="col-lg-12 d-flex justify-content-center">
+          <ul id="portfolio-flters">
+            <li data-filter="*" class="filter-active">imagenes</li>
+            <a href="http://colomboalemanbq.com.co/ntic" target="_black"><br>Instalaciones</a>
+          </ul>
+        </div>
+      </div>
+
+      <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="400">
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+          <div class="portfolio-wrap">
+            <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4></h4>
+              <p></p>
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+          <div class="portfolio-wrap">
+            <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4></h4>
+              <p></p>
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 2"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+          <div class="portfolio-wrap">
+            <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4></h4>
+              <p></p>
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
+                <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section><!-- End Portfolio Section -->
+
+
+   <!-- ======= Team Section ======= -->
+   <section id="team" class="team section-bg">
+    <div class="container">
+
+      <div class="section-title" data-aos="fade-up">
+        <h2>Team</h2>
+        <p>Equipo de creacion e innovacion de proyectos elites</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+          <div class="member" data-aos="fade-up" data-aos-delay="100">
+            <div class="member-img">
+              <div class="social">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="300">
+              <div class="member-img">
+                <img src="assets/img/team/team-3.jpg"class="img-fluid" alt="">
+                <div class="social">
+                    <a href="https://www.facebook.com/larson.torres.31?mibextid=ZbWKwL"target="_black" class="facebook"><i class="bi bi-facebook"></i></a>
+                    <a href="https://instagram.com/duncanlarson07?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D"target="_black" class="instagram"><i class="bi bi-instagram"></i></a>
+                    <a href="https://wa.me/qr/R2W67MWKFZ7RH1"target="_black" class="whatsapp"><i class="bi bi-whatsapp"></i></a>
+                    <a href="https://twitter.com/larsontorres_?t=7C8tqMfPH-WUcDyryZtBTw&s=09"target="_black" class="whatsapp"><i class="bi bi-twitter"></i></a>
+                  </div>
+              </div>
+              <div class="member-info">
+                <h4>Larson Torres</h4>
+                <span>Chief Operating Officer</span>
+                <span>SENA TIC</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div class="member-img">
+                <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
+                <div class="social">
+                  <a href="https://www.facebook.com/profile.php?id=100089637101635" target="_black"><i class="bi bi-facebook"></i></a>
+                  <a href="https://instagram.com/roberto_estrada_55?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D"target="_black"><i class="bi bi-instagram"></i></a>
+                  <a href="https://web.whatsapp.com/"target="_black" class="whatsapp"><i class="bi bi-whatsapp"></i></a>
+                </div>
+              </div>
+              <div class="member-info">
+                <h4>Roberto estrada</h4>
+                <span>Chief Executive Officer</span>
+                <span>SENA TIC</span>
+              </div>
+            </div>
+          </div>
+        </section><!-- End Team Section --><br>
+        <br>
+        <br>
+        <br>
+
+
+
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+        <div class="container">
+
+          <div class="section-title" data-aos="fade-up">
+            <h2>Contact Us</h2>
+          </div>
+
+          <div class="row">
+
+            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+              <div class="contact-about">
+                <h3>Roberto Estrada</h3>
+                <p>contactame via, facebook,whatsapp,instagram .</p>
+                <div class="social-links">
+                  <a href="https://www.facebook.com/profile.php?id=100089637101635"target="_black" class="facebook"><i class="bi bi-facebook"></i></a>
+                  <a href="https://instagram.com/roberto_estrada_55?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D"target="_black" class="instagram"><i class="bi bi-instagram"></i></a>
+                  <a href="https://web.whatsapp.com/"target="_black" class="whatsapp"><i class="bi bi-whatsapp"></i></a>
+                </div>
+                <h3>Larson Torres</h3>
+                  <p>contactame via, facebook,whatsapp,instagram y twitter .</p>
+                  <div class="social-links">
+                    <a href="https://www.facebook.com/larson.torres.31?mibextid=ZbWKwL"target="_black" class="facebook"><i class="bi bi-facebook"></i></a>
+                    <a href="https://instagram.com/duncanlarson07?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D"target="_black" class="instagram"><i class="bi bi-instagram"></i></a>
+                    <a href="https://wa.me/qr/R2W67MWKFZ7RH1"target="_black" class="whatsapp"><i class="bi bi-whatsapp"></i></a>
+                    <a href="https://twitter.com/larsontorres_?t=7C8tqMfPH-WUcDyryZtBTw&s=09"target="_black" class="whatsapp"><i class="bi bi-twitter"></i></a>
+                  </div>
+              </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="200">
+              <div class="info">
+                <div>
+                  <i class="ri-map-pin-line"></i>
+                  <p>colombia<br>barraquilla, carrera 1 c # 11 d **</p>
+                </div>
+
+                <div>
+                  <i class="ri-mail-send-line"></i>
+                  <p>robertoestradao55@gmail.com</p>
+                  <i class="ri-phone-line"></i>
+                  <p>+57 3014240194</p>
+                </div>
+
+                <div>
+                    <i class="ri-mail-send-line"></i>
+                    <p>duncanlarson07@gmail.com</p>
+                    <i class="ri-phone-line"></i>
+                    <p>+57 3007659959</p>
+                  </div>
+
+
+
+
+              </div>
+            </div>
+
+            {{-- <div class="col-lg-5 col-md-12" data-aos="fade-up" data-aos-delay="300">
+              <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                <div class="form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+                <div class="form-group">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+                </div>
+                <div class="form-group">
+                  <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                </div>
+                <div class="my-3">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+                </div>
+                <div class="text-center"><button type="submit">Send Message</button></div>
+              </form>
+            </div> --}}
+
+          </div>
+
+        </div>
+      </section><!-- End Contact Section -->
+      <br>
+      <br>
+
+    </main><!-- End #main -->
+
+
+@stop
